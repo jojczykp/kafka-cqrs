71e2379e7cb2b37041d1994b10dd1907c0e7ce19
jojczykp <pawel.jojczyk@gmail.com>
2018-01-11 23:57:04 +0000

    EC2 and SG setup

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # Prerequisites
 - Terraform
-- Java 8
-- Gradle
 - AWS account
 
+
 # Run
+## Terraform
+- cd terraform
+- ssh-keygen -t rsa -C "kafka-key" -P '' -f ssh_keys/kafka-key
+- terraform init
 - terraform plan
 - terraform apply
+- terraform show
+- cd ..
+
+# Inspect
+cd terraform
+ssh -i ssh_keys/kafka-key ubuntu@\<IP\>
+cd ..
+
+## Application
 - gradle init
+
+
+# Shutdown
+- cd terraform
+- terraform destroy
+- rm ssh_keys/*
+- cd ..
+
+# References
+- https://github.com/hashicorp/atlas-examples/blob/master/setup/ssh.md
--- terraform/kafka.tf
+++ terraform/kafka.tf
@@ -1,11 +1,45 @@
-provider "aws" {
-  //  Use default credentials, i.e. from ~/.aws/credentials
-  //  access_key = "ACCESS_KEY_HERE"
-  //  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+// Use default credentials, i.e. from ~/.aws/credentials
+//variable "access_key" {}
+//variable "secret_key" {}
+variable "region" {
+  default = "eu-west-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-80ca47e6"
-  instance_type = "t2.micro"
+provider "aws" {
+//  access_key = "${var.access_key}"
+//  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_security_group" "kafka_sg" {
+  name = "kafka_sg"
+  description = "Security group for kafka servers"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "kafka_key" {
+  key_name = "kafka-key"
+  public_key = "${file("ssh_keys/kafka-key.pub")}"
+}
+
+resource "aws_instance" "kafka_ec2" {
+  count           = 1
+  ami             = "ami-c145c1b8" // Ubuntu 17.04
+  instance_type   = "m3.medium"
+  security_groups = ["${aws_security_group.kafka_sg.name}"]
+  key_name = "${aws_key_pair.kafka_key.key_name}"
+}
+
+resource "aws_eip" "kafka_ip" {
+  instance = "${aws_instance.kafka_ec2.id}"
+}
+
+output "kafka_ip" {
+  value = "${aws_eip.kafka_ip.public_ip}"
 }
