f5de87facbbe18b1340f0cd8a17257a7faed5a8b
jojczykp <pawel.jojczyk@gmail.com>
2018-06-02 18:29:19 +0100

    e2e tests skeleton project

---
+++ e2e-tests/src/test/groovy/pl/jojczykp/kafka_cqrs/e2e_tests/CreateReadSpec.groovy
@@ -0,0 +1,12 @@
+import spock.lang.Specification
+
+class CreateReadSpec extends Specification {
+
+    def "should read created document"() {
+        when:
+            println("!!! TEST !!!")
+
+        then:
+            true
+    }
+}
