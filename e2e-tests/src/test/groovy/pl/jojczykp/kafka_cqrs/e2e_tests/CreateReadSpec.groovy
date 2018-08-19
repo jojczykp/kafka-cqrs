d77cd8fa431c1ec6ca4134b545e31d36cefe9a77
jojczykp <pawel.jojczyk@gmail.com>
2018-08-19 22:19:29 +0100

    Basic e2e test

--- e2e-tests/src/test/groovy/pl/jojczykp/kafka_cqrs/e2e_tests/CreateReadSpec.groovy
+++ e2e-tests/src/test/groovy/pl/jojczykp/kafka_cqrs/e2e_tests/CreateReadSpec.groovy
@@ -1,12 +1,30 @@
+package pl.jojczykp.kafka_cqrs.e2e_tests
+
+import groovyx.net.http.RESTClient
 import spock.lang.Specification
+
 
 class CreateReadSpec extends Specification {
 
     def "should read created document"() {
+        given:
+            def client = new RESTClient('http://minikube.local')
+            client.encoder['application/vnd.kafka-cqrs.create-document.1+json'] = client.encoder['application/json']
+
         when:
-            println("!!! TEST !!!")
+            def response = client.post(
+                    path: '/producer/documents',
+                    contentType: 'application/vnd.kafka-cqrs.create-document.1+json',
+                    headers: [
+                            'Content-Type': 'application/vnd.kafka-cqrs.create-document.1+json'
+                    ],
+                    body: [
+                            'author':'Author1',
+                            'text':'Some Text'
+                    ]
+            )
 
         then:
-            true
+            response.statusLine.statusCode == 201
     }
 }
