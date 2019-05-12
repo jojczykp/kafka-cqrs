7c46ec5fa3182ba638915aeddaeaa3818d8d08f5
jojczykp <pawel.jojczyk@gmail.com>
2019-05-12 22:44:07 +0100

    Delete unused terraform folder

--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "kafka_ssh" {
-  value = "ssh -i ${var.kafka_user["private_key_path"]} ${var.kafka_user["name"]}@${aws_eip.kafka_ip.public_ip}"
-}
-
-output "kafka_verify" {
-  value = "telnet ${aws_eip.kafka_ip.public_ip} ${var.kafka_port}"
-}
--- terraform/setup-scripts/setup-java.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "...:: Setup Java ::..."
-
-apt-get update
-apt-get install -y default-jre
-java -version
-
-echo "...:: Setup Java - Done ::..."
