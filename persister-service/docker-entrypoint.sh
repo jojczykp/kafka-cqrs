e4a3b90ea7b66123925fc281da3dd817bc19a99a
jojczykp <pawel.jojczyk@gmail.com>
2022-11-15 14:04:20 +0000

    Move waiting to init containers

--- persister-service/docker-entrypoint.sh
+++ persister-service/docker-entrypoint.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
 
-log() {
-    echo "[$(date)] ${1}"
-}
-
-wait_for_open_port () {
-    HOST=${1}
-    PORT=${2}
-
-    log "Waiting for ${HOST}:${PORT} to open..."
-
-    until nc -w 5 -z "${HOST}" "${PORT}"
-    do
-        sleep 1
-        true
-    done
-
-    log "OK!"
-}
-
-
-log "Starting container"
-
-wait_for_open_port cassandra-service 9042
-wait_for_open_port kafka-service 9092
-
-log "Running Application"
-
 java -jar persister-service.jar
--- reader-service/docker-entrypoint.sh
+++ reader-service/docker-entrypoint.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-log() {
-    echo "[$(date)] ${1}"
-}
-
-wait_for_opened_port () {
-    HOST=${1}
-    PORT=${2}
-
-    log "Waiting for ${HOST}:${PORT} to open..."
-
-    until nc -w 5 -z "${HOST}" "${PORT}"
-    do
-        sleep 1
-        true
-    done
-
-    log "OK!"
-}
-
-
-log "Starting container"
-
-wait_for_opened_port cassandra-service 9042
-
-log "Running Application"
-
 java -jar reader-service.jar
